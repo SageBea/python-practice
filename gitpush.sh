b1568ca481223763892ebeff20f8f3a403490a1e
SageBea <Daishavu2@icloud.com>
2025-09-26 15:27:29 -0500

    testing saving

--- gitpush.sh
+++ gitpush.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Quick Git Push Script
 
+repo=$(basename `git rev-parse --show-toplevel`)
+echo "Repo: $repo"
+
 echo "Enter commit message:"
 read msg
 
 git add .
 git commit -m "$msg"
 git push
-
-
-
