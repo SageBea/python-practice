315d500b1051e5f33b418bf01ed7ee83976cc74e
SageBea <Daishavu2@icloud.com>
2025-10-29 19:29:19 -0500

    done learning and adding to syntax. next is statements.

--- Basics/syntax/MVRank.py
+++ Basics/syntax/MVRank.py
@@ -1,2 +1,22 @@
-# A mod
+# A modest list of some QT movies I love... Just playing arond with variables here.
 
+movie1 = "Django"
+movie2 = "Kill Bill Vol. 1 & 2"
+movie3 = "Four Rooms"
+movie4 = "The Hatefull Eight"
+
+print("Find out what my favorite movies are!")
+print("Type 1, 2, 3, or 4 to see their rank.")
+x = input ("enter your choice #")
+
+if x == "1":
+    print(""+movie1+" is my number 1 favorite movie!")
+
+if x == "2":
+    print(""+movie2+" is my 2nd favorite movie!")
+
+if x == "3":
+    print(""+movie3+" is my 3rd favorite movie!")
+
+if x == "4":
+    print (""+movie4+" is my 4th favorite movie!")
